544f7a833f1a1a5aba593f37a2c138ab89cc3cb2
juniordeveu <tomrys@juniordev.eu>
2023-02-06 21:19:21 +0100

    added page FAQ with js

---
+++ public/js/collasible.js
@@ -0,0 +1,23 @@
+
+let getElementCollasible = document.getElementsByClassName( 'box-collasible')
+
+for( let i = 0; i < getElementCollasible.length; i++){
+    getElementCollasible[i].addEventListener( "click", function() {
+        let arrow = document.querySelectorAll( ".arrow" );
+        let boxShadow = document.querySelectorAll( ".box-collasible" )
+        boxShadow[i].classList.toggle( "addShadow" )
+        arrow[i].classList.toggle( "animArrow" );
+        getElementCollasible[i].classList.toggle( "changeColor" )
+        var content = document.getElementsByClassName( 'collasible-content' )[i]
+        if (content.style.maxHeight){
+          content.style.maxHeight = null;
+        } else {
+          content.style.maxHeight = content.scrollHeight + "px";
+        } 
+    })
+
+}
+
+
+
+
