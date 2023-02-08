82c26093e01c923220413d671956e6fd20277ae0
juniordeveu <tomrys@juniordev.eu>
2023-02-08 18:50:26 +0100

    added visitor counter

--- controllers/portfolio.js
+++ controllers/portfolio.js
@@ -1,7 +1,36 @@
+const fs = require ( 'fs' );
+const { dirname } = require( 'path' );
+const path = require( 'path' );
+const getPathCount = path.join( path.dirname( require.main.filename ), 'data', 'count.txt' );
 
+fs.readFile( getPathCount, ( err, actualNumber ) => {
+    let parseVisit = parseInt( actualNumber )
+    sumVisits.push( parseVisit )    
+})
+
+let couting = 0
+let sumVisits = [];
 
 let portfolio = ( req, res ) => {
-    res.render( 'portfolio.ejs' );
+    if ( req.url == '/' ) {
+        fs.readFile( getPathCount, ( err, contentCount ) => {
+            let sum = parseInt( contentCount ) + parseInt( couting )
+            fs.writeFile( getPathCount, sum.toString() , ( err ) => {
+            sumVisits.push( sum )
+            })
+         
+        })
+    if (couting == 1){
+        couting--
+    }
+    couting++;
+        
+
+    }
+    let vistsOnPage = sumVisits.pop() + 1;
+    res.render( 'portfolio.ejs' ,{
+        visits: vistsOnPage
+    });
     return res.end();
 }
 
